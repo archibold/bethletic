209c6bd9b67b96c7250273cbb8cd26041bc767a8
Piotr Borysowski <piotr.borysowski@gmail.com>
2024-07-22 14:20:55 +0200

    add confirmation email registration

--- src/lib/verification/mail.ts
+++ src/lib/verification/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
-const mail_domain = process.env.RESEND_MAIL;
+const mail_domain = process.env.RESEND_MAIL || "";
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     await resend.emails.send({
